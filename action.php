9a36773f37fb2e0afe1aaf99941cee1eca3564a7
Bruce Oliveira <bruoliveira@gmail.com>
2018-09-24 21:46:25 -0300

    validação de formulario

--- action.php
+++ action.php
@@ -2,13 +2,35 @@
 require_once 'vendor/autoload.php';
 
 use Endroid\QrCode\QrCode;
+use Respect\Validation\Validator as v;
 
-$qr = $_POST['nome'];
+$nasc = $_POST['nasc'];
 
-$qrCode = new QrCode($qr);
+$email = $_POST['email'];
 
-header('Content-Type: '.$qrCode->getContentType());
-echo $qrCode->writeString();
+$vNasc = v::date()->validate($nasc); // true
+$vEmail = v::email()->validate($email);
+
+
+if (!$vNasc) {
+	echo "erro na data de nascimento";
+	exit();
+	// echo "concluido com sucesso ".$nasc.'<br>';
+}
+
+
+if (!$vEmail) {
+	echo "erro no email";
+	exit();
+}
+
+$v = "nascimento ".$nasc. "email ".$email;
+// var_dump($vNasc, $vEmail);
+
+ $qrCode = new QrCode($v);
+
+ header('Content-Type: '.$qrCode->getContentType());
+ echo $qrCode->writeString();
 
 
 
